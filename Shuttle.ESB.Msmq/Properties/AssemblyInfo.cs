48e5bc56108cdf2a5a5320b04b7f2117e62d7c1a
Eben <me@ebenroux.co.za>
2019-06-21 17:24:06 +0200

    - v11.0.0

--- Shuttle.ESB.Msmq/Properties/AssemblyInfo.cs
+++ Shuttle.ESB.Msmq/Properties/AssemblyInfo.cs
@@ -17,10 +17,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Framework 4.7.1")]
 #endif
 
-[assembly: AssemblyVersion("10.0.5.0")]
-[assembly: AssemblyCopyright("Copyright © Eben Roux 2018")]
+[assembly: AssemblyVersion("11.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © Eben Roux 2019")]
 [assembly: AssemblyProduct("Shuttle.Esb.Msmq")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.5")]
+[assembly: AssemblyInformationalVersion("11.0.0")]
 [assembly: ComVisible(false)]
